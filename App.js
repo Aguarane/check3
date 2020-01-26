0b9058ca43326b1bf6598c6944b1e67385bbad86
Aguarane <56790666+Aguarane@users.noreply.github.com>
2020-01-26 17:16:48 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import './aa.css'
+
+function App() {
+  return (
+    <div className="App">
+   <div style={{border:"solid 1px black", maxWidth:"100vw"}}>
+<h1 class="title red">My title</h1>
+<img src="/imageInSrc.jpg" />
+<img src="/imageInPublic.jpg" />
+    </div>
+    </div>
+
+  );
+}
+
+export default App
